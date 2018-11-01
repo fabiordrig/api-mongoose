c101539e4a0fa6db78c410dcdc91cdce1e148299
Fabio Rodrigues <fabiordrig@gmail.com>
2018-10-31 23:28:22 -0300

    feature - construindo router

--- src/app.js
+++ src/app.js
@@ -6,41 +6,18 @@ const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router();
 
+//carrega rotas
+const index = require('./routes/index')
+const products = require('./routes/products')
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ 
+  extended: false
+}))
 
 
-let get = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: 'API com Express e Mongo',
-    version: '0.0.2'
-  })
-})
-let post = router.post('/', (req, res, next) => {
-  res.status(201).send(req.body)
-})
-
-let put = router.put('/:id', (req, res, next) => {
-  let id = req.params.id
-  res.status(200).send({
-    id: id,
-    item: req.body
-  })
-})
-
-let deleted = router.delete('/:id', (req, res, next) => {
-  let id = req.params.id
-  res.status(200).send({
-    id: id,
-    item: req.body
-  })
-})
-
-app.use('/', get)
-app.use('/products', post)
-app.use('/products', put)
-app.use('/products', deleted)
+app.use('/', index)
+app.use('/products', products)
 
 
 module.exports = app
