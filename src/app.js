351bf61fff34b1664038721d66faca7c07e96893
Fabio Rodrigues <fabiordrig@gmail.com>
2018-11-03 00:02:40 -0300

    fix - limpando router, add package mongoose, add mongoose ao app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 'use strict'
 
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 const app = express()
 const router = express.Router();
 
+
+mongoose.connect('mongodb://fabio:fabinho123@ds041177.mlab.com:41177/node-train')
+
+
 //carrega rotas
 const index = require('./routes/index')
 const products = require('./routes/products')
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ 
