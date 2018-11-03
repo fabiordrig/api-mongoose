34b2f19874cb32dbf0f520ce59e1d9410bb0e7a3
Fabio Rodrigues <fabiordrig@gmail.com>
2018-11-03 18:38:27 -0300

    fix - corrigindo nome das pastas

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router();
-const controller = require('../controllers/productsController')
+const controller = require('../controllers/products')
 
 router.post('/', controller.post)
 router.put('/:id',controller.put )
